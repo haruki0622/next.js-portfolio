d3cecb8900e93598a84a507bf2f4a7148f044470
Haruki Izumihara <izu.0622h@gmail.com>
2022-03-17 19:28:59 +0900

    changed main-title

--- components/Header.js
+++ components/Header.js
@@ -3,12 +3,12 @@ import Image from 'next/image';
 
 export default function Header() {
   return (
-    <header className='p-5 xl:w-[70%] xl:m-auto'>
+    <header className='px-5 xl:w-[70%] xl:m-auto'>
       <div className='flex items-center justify-between'>
         <Link href='/' passHref>
           <h1>
             <a className='relative block font-bold xl:text-[1.4rem] hover:cursor-pointer'>
-              haruharu
+              <Image src='/logo1.png' alt='logo' width={100} height={50} ></Image>
             </a>
           </h1>
         </Link>
@@ -16,7 +16,7 @@ export default function Header() {
           <ul>
             <eli>
               <Link href='/about'>
-                <a className='relative block w-[130px] xl:w-[130px] h-[30px] font-bold hover:cursor-pointer'>
+                <a className='relative block h-[30px] hover:cursor-pointer'>
                   About
                 </a>
               </Link>
--- components/Main.js
+++ components/Main.js
@@ -3,7 +3,7 @@ import Image from 'next/image';
 export default function Main() {
   return (
     <div className='py-60 flex flex-col items-center xl:flex-row bg-bg-image bg-cover bg-no-repeat bg-fixed justify-center'>
-      <h2 className='mt-5 inline-block text-4xl text-white font-bold leading-[1.5] xl:text-[5rem] xl:leading-[1.3]'>
+      <h2 className='mt-5 inline-block text-4xl text-white font-bold leading-[1.5] xl:text-[5rem] xl:leading-[1.5]'>
         驚きをとどけて
         <br />
         あたなのビジネス
